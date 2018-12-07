5cad9e2ef4fa511174adfc7e505aa6318ab50f91
Peter Gilbert <pgilbert@oasislabs.com>
2018-12-07 20:32:51 +0000

    Get git info from CI env variables

--- src/report/coveralls.rs
+++ src/report/coveralls.rs
@@ -4,6 +4,43 @@ use crate::traces::{CoverageStat, TraceMap};
 use coveralls_api::*;
 use log::info;
 use std::collections::HashMap;
+use std::env;
+
+fn get_git_info() -> Option<GitInfo> {
+    if env::var_os("CIRCLECI").is_some() {
+        let head = Head {
+            id: env::var("CIRCLE_SHA1").unwrap_or(String::new()),
+            author_name: env::var("CIRCLE_USERNAME").unwrap_or(String::new()),
+            author_email: String::new(),
+            committer_name: env::var("CIRCLE_USERNAME").unwrap_or(String::new()),
+            committer_email: String::new(),
+            message: String::new(),
+        };
+        Some(GitInfo {
+            branch: env::var("CIRCLE_BRANCH").unwrap_or(String::new()),
+            remotes: vec![],
+            head: head,
+        })
+    }
+    else if env::var_os("BUILDKITE").is_some() {
+        let head = Head {
+            id: env::var("BUILDKITE_COMMIT").unwrap_or(String::new()),
+            author_name: env::var("BUILDKITE_BUILD_CREATOR").unwrap_or(String::new()),
+            author_email: env::var("BUILDKITE_BUILD_CREATOR_EMAIL").unwrap_or(String::new()),
+            committer_name: env::var("BUILDKITE_BUILD_CREATOR").unwrap_or(String::new()),
+            committer_email: env::var("BUILDKITE_BUILD_CREATOR_EMAIL").unwrap_or(String::new()),
+            message: env::var("BUILDKITE_MESSAGE").unwrap_or(String::new()),
+        };
+        Some(GitInfo {
+            branch: env::var("BUILDKITE_BRANCH").unwrap_or(String::new()),
+            remotes: vec![],
+            head: head,
+        })
+    }
+    else {
+        None
+    }
+}
 
 pub fn export(coverage_data: &TraceMap, config: &Config) -> Result<(), RunError> {
     if let Some(ref key) = config.coveralls {
@@ -15,6 +52,9 @@ pub fn export(coverage_data: &TraceMap, config: &Config) -> Result<(), RunError>
             _ => Identity::RepoToken(key.clone()),
         };
         let mut report = CoverallsReport::new(id);
+        if let Some(info) = get_git_info() {
+            report.set_detailed_git_info(info);
+        }
         for file in &coverage_data.files() {
             let rel_path = config.strip_project_path(file);
             let mut lines: HashMap<usize, usize> = HashMap::new();
